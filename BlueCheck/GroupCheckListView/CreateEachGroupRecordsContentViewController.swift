cf2f48a3459c4f693e8c4459cb1c3771e11269b1
DevKDuck <loading95@naver.com>
2023-02-07 21:01:43 +0900

    CreateEachGroupRecordsContentViewController - UIDatePickerView, UIImagePickerView 추가

--- BlueCheck/GroupCheckListView/CreateEachGroupRecordsContentViewController.swift
+++ BlueCheck/GroupCheckListView/CreateEachGroupRecordsContentViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CreateEachGroupRecordsContentViewController: UIViewController{
     
@@ -40,60 +41,67 @@ class CreateEachGroupRecordsContentViewController: UIViewController{
     let titleTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "제목을 작성해보세요"
-        textField.backgroundColor = .white
+        textField.backgroundColor = UIColor(hue: 0.5944, saturation: 0.34, brightness: 1, alpha: 1.0)
         textField.textColor = .darkGray
         
         return textField
     }()
     
-    let dateButton : UIButton = {
-        let button = UIButton()
-        button.setTitle("날짜", for: .normal)
-        button.setTitleColor(.darkGray, for: .normal)
-        return button
+    let startLabel : UILabel = {
+        let label = UILabel()
+        label.text = "시작 날짜"
+        label.textColor = .systemBlue
+        return label
     }()
+    
+    private let startDatePicker = UIDatePicker()
+    
+    let endLabel : UILabel = {
+        let label = UILabel()
+        label.text = "종료 날짜"
+        label.textColor = .systemRed
+        return label
+    }()
+    
+    private let endDatePicker = UIDatePicker()
     
     let recordImage: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "xmark")
+        imageView.image = UIImage(systemName: "figure.tennis")
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = UIColor(hue: 0.5944, saturation: 0.34, brightness: 1, alpha: 1.0)
+        
         return imageView
     }()
-//    let barView: UIView = {
-//        let barview = UIView()
-//        barview.backgroundColor = .darkGray
-//        return barview
-//    }()
     
     
-//    lazy var titleStackView: UIStackView = {
-//        let stackView = UIStackView(arrangedSubviews: [titleTextField,barView])
-//        stackView.axis = .vertical
-//        stackView.spacing = 3
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//        return stackView
-//    }()
-//
-//    lazy var dateStackView: UIStackView = {
-//        let stackView = UIStackView(arrangedSubviews: [dateButton,barView])
-//        stackView.axis = .vertical
-//        stackView.spacing = 3
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//        return stackView
-//    }()
-//
-//    lazy var recordImageStackView: UIStackView = {
-//        let stackView = UIStackView(arrangedSubviews: [recordImage,barView])
-//        stackView.axis = .vertical
-//        stackView.spacing = 3
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//        return stackView
-//    }()
+    let titlebarView: UIView = {
+        let barview = UIView()
+        barview.backgroundColor = UIColor(hue: 0.5944, saturation: 0.34, brightness: 1, alpha: 1.0)
+        barview.translatesAutoresizingMaskIntoConstraints = false
+        return barview
+    }()
+    
+    let datebarView: UIView = {
+        let barview = UIView()
+        barview.backgroundColor = UIColor(hue: 0.5944, saturation: 0.34, brightness: 1, alpha: 1.0)
+        barview.translatesAutoresizingMaskIntoConstraints = false
+        return barview
+    }()
+    
+    let recordImagebarView: UIView = {
+        let barview = UIView()
+        barview.backgroundColor = UIColor(hue: 0.5944, saturation: 0.34, brightness: 1, alpha: 1.0)
+        barview.translatesAutoresizingMaskIntoConstraints = false
+        return barview
+    }()
+    
     
     var contentTextView: UITextView = {
         let textView = UITextView()
         
-        textView.backgroundColor = .white
-        textView.textInputView.backgroundColor = .white
+        textView.backgroundColor = UIColor(hue: 0.5944, saturation: 0.34, brightness: 1, alpha: 1.0)
+        textView.textInputView.backgroundColor = UIColor(hue: 0.5944, saturation: 0.34, brightness: 1, alpha: 1.0)
         textView.contentInset = .init(top: 0, left: 0, bottom: 0, right: 0)
         textView.textContainerInset = .init(top: 10, left: 10, bottom: 10, right: 10)
         textView.scrollIndicatorInsets = .init(top: 10, left: 10, bottom: 10, right: 20)
@@ -101,19 +109,94 @@ class CreateEachGroupRecordsContentViewController: UIViewController{
         return textView
     }()
     
+    let picker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        
+        imageViewTapGestureAttributes()
+        setAttributesDatePicker()
         setLayoutConstraints()
+        picker.delegate = self
+        
     }
+    
+    private func imageViewTapGestureAttributes(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapRecordImage(_:)))
+        recordImage.addGestureRecognizer(tapGesture)
+        recordImage.isUserInteractionEnabled = true //사용자
+    }
+    
+    
+    @objc func tapRecordImage (_ sender: UIImageView){
+        let alert = UIAlertController(title: "인증 사진 찾기", message: "자랑스럽게 이루어낸 사진을 공유하세요!", preferredStyle: .actionSheet)
+        
+        let camera = UIAlertAction(title: "카메라", style: .default) { camera in
+//            let pickerController = UIImagePickerController()
+//            pickerController.sourceType = .camera
+//            pickerController.allowsEditing = false
+//            pickerController.mediaTypes = ["public.image"]
+////            pickerController.delegate = self
+//            self.present(pickerController, animated: true)
+            self.openCamera()
+        }
+        
+        let album = UIAlertAction(title: "앨범", style: .default) { album in
+            self.openLibrary()
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel) { cancel in
+            alert.dismiss(animated: true)
+        }
+        
+        alert.addAction(camera)
+        alert.addAction(album)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true)
+        
+    }
+    
+    private func setAttributesDatePicker() {
+        startDatePicker.preferredDatePickerStyle = .compact
+        startDatePicker.datePickerMode = .dateAndTime
+        startDatePicker.locale = Locale(identifier: "ko-KR")
+        startDatePicker.timeZone  = .autoupdatingCurrent
+        startDatePicker.addTarget(self, action: #selector(valueChangedDatePicker(_:)), for: .valueChanged)
+        
+        
+        endDatePicker.preferredDatePickerStyle = .automatic
+        endDatePicker.datePickerMode = .dateAndTime
+        endDatePicker.locale = Locale(identifier: "ko-KR")
+        endDatePicker.timeZone  = .autoupdatingCurrent
+        endDatePicker.addTarget(self, action: #selector(valueChangedDatePicker(_:)), for: .valueChanged)
+    }
+    
+    @objc func valueChangedDatePicker(_ sender: UIDatePicker){
+        //시작날짜 종료날짜 변경필요
+        print(sender.date)
+    }
+    
     
     private func setLayoutConstraints(){
         self.view.addSubview(topView)
         self.view.addSubview(completeButton)
         self.view.addSubview(disMissButton)
         self.view.addSubview(titleTextField)
-        self.view.addSubview(dateButton)
+        self.view.addSubview(titlebarView)
+        
+        self.view.addSubview(startLabel)
+        self.view.addSubview(startDatePicker)
+        
+        
+        self.view.addSubview(endLabel)
+        self.view.addSubview(endDatePicker)
+        
+        self.view.addSubview(datebarView)
         self.view.addSubview(recordImage)
+        self.view.addSubview(recordImagebarView)
+        
         self.view.addSubview(contentTextView)
         
         
@@ -122,7 +205,13 @@ class CreateEachGroupRecordsContentViewController: UIViewController{
         completeButton.translatesAutoresizingMaskIntoConstraints = false
         disMissButton.translatesAutoresizingMaskIntoConstraints = false
         titleTextField.translatesAutoresizingMaskIntoConstraints = false
-        dateButton.translatesAutoresizingMaskIntoConstraints = false
+        startLabel.translatesAutoresizingMaskIntoConstraints = false
+        startDatePicker.translatesAutoresizingMaskIntoConstraints = false
+        endLabel.translatesAutoresizingMaskIntoConstraints = false
+        endDatePicker.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
         recordImage.translatesAutoresizingMaskIntoConstraints = false
         contentTextView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -143,22 +232,87 @@ class CreateEachGroupRecordsContentViewController: UIViewController{
             titleTextField.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
             titleTextField.heightAnchor.constraint(equalToConstant: 50),
             
-            dateButton.topAnchor.constraint(equalTo: self.titleTextField.bottomAnchor, constant: 5),
-            dateButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            dateButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            dateButton.heightAnchor.constraint(equalToConstant: 50),
             
-            recordImage.topAnchor.constraint(equalTo: self.dateButton.bottomAnchor, constant: 5),
+            titlebarView.topAnchor.constraint(equalTo: self.titleTextField.bottomAnchor,constant: 5),
+            titlebarView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            titlebarView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            titlebarView.heightAnchor.constraint(equalToConstant: 2),
+            
+            startDatePicker.topAnchor.constraint(equalTo: self.titlebarView.bottomAnchor, constant: 5),
+            startDatePicker.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            
+            startLabel.centerYAnchor.constraint(equalTo: self.startDatePicker.centerYAnchor),
+            startLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            startLabel.widthAnchor.constraint(equalToConstant: self.view.bounds.width / 5),
+            
+            
+            //            startDatePicker.widthAnchor.constraint(equalToConstant: self.view.bounds.width / 2),
+            
+            endDatePicker.topAnchor.constraint(equalTo: self.startDatePicker.bottomAnchor, constant: 5),
+            endDatePicker.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            
+            endLabel.centerYAnchor.constraint(equalTo: self.endDatePicker.centerYAnchor),
+            endLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            endLabel.widthAnchor.constraint(equalToConstant: self.view.bounds.width / 5),
+            
+            
+            //            endDatePicker.widthAnchor.constraint(equalToConstant: self.view.bounds.width / 3),
+            
+            
+            datebarView.topAnchor.constraint(equalTo: self.endDatePicker.bottomAnchor,constant: 5),
+            datebarView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            datebarView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            datebarView.heightAnchor.constraint(equalToConstant: 2),
+            
+            
+            recordImage.topAnchor.constraint(equalTo: self.datebarView.bottomAnchor, constant: 5),
             recordImage.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
             recordImage.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
             recordImage.heightAnchor.constraint(equalToConstant: self.view.bounds.height / 3),
             
-            contentTextView.topAnchor.constraint(equalTo: self.recordImage.bottomAnchor,constant: 5),
+            recordImagebarView.topAnchor.constraint(equalTo: self.recordImage.bottomAnchor,constant: 5),
+            recordImagebarView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            recordImagebarView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            recordImagebarView.heightAnchor.constraint(equalToConstant: 2),
+            
+            
+            contentTextView.topAnchor.constraint(equalTo: self.recordImagebarView.bottomAnchor,constant: 5),
             contentTextView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
             contentTextView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
             contentTextView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
             
         ])
         
+    }
+}
+
+
+extension CreateEachGroupRecordsContentViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    func openCamera(){
+        picker.sourceType = .camera
+        present(picker, animated: false)
+    }
+    
+    func openLibrary(){
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false)
+    }
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            picker.dismiss(animated: true)
+            return
+        }
+        self.recordImage.image = image
+        picker.dismiss(animated: true, completion: nil)
+        // 비디오인 경우 - url로 받는 형태
+        //    guard let url = info[UIImagePickerController.InfoKey.mediaURL] as? URL else {
+        //      picker.dismiss(animated: true, completion: nil)
+        //      return
+        //    }
+        //    let video = AVAsset(url: url)
     }
 }
