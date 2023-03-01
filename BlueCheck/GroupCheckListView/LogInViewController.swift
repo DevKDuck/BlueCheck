8b17cc15dd5a78493839254c5bca6d0fb4aabb69
DevKDuck <loading95@naver.com>
2023-03-01 19:02:19 +0900

    Apple SignIn 설정

--- BlueCheck/GroupCheckListView/LogInViewController.swift
+++ BlueCheck/GroupCheckListView/LogInViewController.swift
@@ -10,6 +10,8 @@ import Firebase
 import FirebaseAuth
 import FirebaseFirestore
 import FirebaseFirestoreSwift
+import AuthenticationServices
+import CryptoKit
 
 class LogInViewController: UIViewController{
     
@@ -265,6 +267,7 @@ class LogInViewController: UIViewController{
     let kakaotalkButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "square"), for: .normal)
+        
         return button
     }()
     
@@ -275,12 +278,17 @@ class LogInViewController: UIViewController{
         return button
     }()
     
-    let appleButton: UIButton = {
+    lazy var appleButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "square"), for: .normal)
         button.contentMode = .scaleToFill
+        button.addTarget(self, action: #selector(tapAppleButton(_:)), for: .touchUpInside)
         return button
     }()
+    
+    @objc func tapAppleButton(_ sender: UIButton){
+        startSignInWithAppleFlow()
+    }
     
     lazy var sNSButtonStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [kakaotalkButton,naverButton,appleButton])
@@ -417,5 +425,116 @@ class LogInViewController: UIViewController{
             
             
         ])
+    }
+    fileprivate var currentNonce: String?
+}
+
+extension LogInViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print ("Error Apple sign in: %@", error)
+                    return
+                }
+                // User is signed in to Firebase with Apple.
+                // ...
+                
+                let data = ["email" : authResult?.user.email, "name" : "애플로그인으로 없음", "uid" : authResult?.user.uid]
+                
+                
+                Firestore.firestore().collection("user").document(authResult?.user.email ?? "").setData(data as [String : Any]){ error in
+                    if let error = error{
+                        print("DEBUG:\(error.localizedDescription)")
+                        return
+                    }
+                }
+                
+                let tabbarViewController = TabbarViewController()
+                tabbarViewController.currentUserEmail = authResult?.user.email ?? ""
+                tabbarViewController.modalPresentationStyle = .fullScreen
+                self.present(tabbarViewController, animated: true)
+            }
+        }
+    }
+}
+
+//Apple Sign in
+extension LogInViewController {
+    func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+    
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+}
+
+extension LogInViewController : ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
     }
 }
